da412a88996b0e5ed652259759f7c5d21674d1af
agung <agungpermadi13@gmail.com>
2020-05-02 09:23:28 +0700

    perulangan

--- .vscode/src/perulangan.py
+++ .vscode/src/perulangan.py
@@ -11,3 +11,26 @@ for f in fruits:
 animals = ["cow", "cat", "snack", "chicken"]
 for index in range(len(animals)):
     print("animal {}".format(animals[index]))
+
+# perulangan while
+count = 0
+while count < 5:
+    print("count is {}".format(count))
+    count = count + 1
+
+# break
+for letter in "Indonesia":
+    if letter == "e":
+        break
+    print("current letter {}".format(letter))
+
+var = 30
+while var > 0:
+    print("current variabel value {}".format(var))
+    var = var - 1
+    if var == 5:
+        break
+
+#fungsi else setelah for
+
+
